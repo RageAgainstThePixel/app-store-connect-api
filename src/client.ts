c6dfc880cdbe4d7e24ef7034a96d49feba2bcbaf
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-02-04 10:53:48 -0500

    app-store-connect-api@v3.7.1
    - Added support for individual keys

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import * as services from './app_store_connect_api/services.gen';
-import * as jose from "jose";
+import * as jose from 'jose';
 import fs from 'fs';
 
 /**
@@ -55,27 +55,32 @@ class AppStoreConnectClient {
     if (this.appStoreConnectOptions.bearerToken) {
       this.bearerToken = this.appStoreConnectOptions.bearerToken;
     } else {
-      if (this.appStoreConnectOptions.privateKeyId &&
-        this.appStoreConnectOptions.issuerId &&
-        (this.appStoreConnectOptions.privateKey || this.appStoreConnectOptions.privateKeyFile)) {
+      const { privateKeyId, issuerId, privateKey, privateKeyFile, expirationTime } = this.appStoreConnectOptions;
+      if (privateKeyId && (privateKey || privateKeyFile)) {
         const defaultExpirationTime = 600; // 10 minutes
-        const expirationTime = this.appStoreConnectOptions.expirationTime ?? defaultExpirationTime;
-        if (!this.bearerToken || this.bearerTokenGeneratedAt + expirationTime * 1000 < Date.now()) {
-          if (this.appStoreConnectOptions.privateKeyFile) {
-            const fileHandle = await fs.promises.open(this.appStoreConnectOptions.privateKeyFile, 'r');
+        const tokenExpirationTime = expirationTime ?? defaultExpirationTime;
+        if (!this.bearerToken || this.bearerTokenGeneratedAt + tokenExpirationTime * 1000 < Date.now()) {
+          if (privateKeyFile) {
+            const fileHandle = await fs.promises.open(privateKeyFile, fs.constants.O_RDONLY);
             try {
               this.appStoreConnectOptions.privateKey = await fileHandle.readFile('utf8');
-            }
-            finally {
+            } finally {
               await fileHandle.close();
             }
           }
-          this.bearerToken = await this.generateAuthToken(
-            this.appStoreConnectOptions.issuerId,
-            this.appStoreConnectOptions.privateKeyId,
-            this.appStoreConnectOptions.privateKey!,
-            expirationTime,
-          );
+          if (issuerId) {
+            this.bearerToken = await this.generateAuthToken(
+              issuerId,
+              privateKeyId,
+              this.appStoreConnectOptions.privateKey!,
+              tokenExpirationTime);
+          } else {
+            this.bearerToken = await this.generateUserAuthToken(
+              privateKeyId,
+              this.appStoreConnectOptions.privateKey!,
+              tokenExpirationTime);
+          }
+          this.bearerTokenGeneratedAt = Date.now();
         }
       } else {
         throw new Error('Bearer token or private key information is required to generate a token');
@@ -90,12 +95,28 @@ class AppStoreConnectClient {
    * @see https://developer.apple.com/documentation/appstoreconnectapi/generating_tokens_for_api_requests
   */
   private async generateAuthToken(issuerId: string, privateKeyId: string, privateKey: string, expirationTime: number): Promise<string> {
-    const alg = "ES256";
+    const alg = 'ES256';
     const key = await jose.importPKCS8(privateKey, alg);
     const token = await new jose.SignJWT({})
-      .setProtectedHeader({ alg, kid: privateKeyId, typ: "JWT" })
+      .setProtectedHeader({ alg, kid: privateKeyId, typ: 'JWT' })
       .setIssuer(issuerId)
-      .setAudience("appstoreconnect-v1")
+      .setAudience('appstoreconnect-v1')
+      .setExpirationTime(new Date(Date.now() + expirationTime * 1000))
+      .sign(key);
+    return token;
+  }
+
+  /**
+   * Generates a user auth token for authenticating with the App Store Connect API.
+   * @see https://developer.apple.com/documentation/appstoreconnectapi/generating-tokens-for-api-requests#Create-the-JWT-Payload-for-Individual-Keys
+   */
+  private async generateUserAuthToken(privateKeyId: string, privateKey: string, expirationTime: number): Promise<string> {
+    const alg = 'ES256';
+    const key = await jose.importPKCS8(privateKey, alg);
+    const token = await new jose.SignJWT({})
+      .setProtectedHeader({ alg, kid: privateKeyId, typ: 'JWT' })
+      .setSubject('user')
+      .setAudience('appstoreconnect-v1')
       .setExpirationTime(new Date(Date.now() + expirationTime * 1000))
       .sign(key);
     return token;
